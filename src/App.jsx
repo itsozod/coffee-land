e07277ccd3ca52de4b4c3f1a855b7bc5750f67d4
Ozod <o.bukhorizoda@mail.ru>
2023-12-04 00:40:14 +0500

    set the loading gif when the page is loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const loadImage = "src/assets/coffee-gif.gif";
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    setLoader(true);
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <div className="loader"></div>
-      <h1>Hello, React!</h1>
+      {loader ? (
+        <div className="loaderContainer">
+          <img className="loader" src={loadImage} alt="loader" />
+        </div>
+      ) : (
+        <h1>Hello, React!</h1>
+      )}
     </>
   );
 }
