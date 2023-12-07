5c7d9647efc4bdf4ad0241341fda8b79b45a1ae9
Ozod <o.bukhorizoda@mail.ru>
2023-12-07 23:35:48 +0500

    working with sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import { Loader } from "./components/loader/Loader";
+
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { Home } from "./pages/home/Home";
+import { About } from "./pages/about/About";
+import { Menu } from "./pages/menu/Menu";
 
 function App() {
   const [loader, setLoader] = useState(false);
 
-  const showLoader = () => {
+  const showLoader = async () => {
     setLoader(true);
     setTimeout(() => {
       setLoader(false);
@@ -15,7 +21,21 @@ function App() {
   useEffect(() => {
     showLoader();
   }, []);
-  return <>{loader ? <Loader /> : <h1>Hello, React!</h1>}</>;
+  return (
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/menu" element={<Menu />}></Route>
+          </Routes>
+        </Sidebar>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,53 @@
-export const Sidebar = () => {
-  return;
+/* eslint-disable react/prop-types */
+import { NavLink } from "react-router-dom";
+import styles from "./Sidebar.module.css";
+import { FaHome } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+export const Sidebar = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <div className={styles.top_section}>
+          <h1>Logo</h1>
+          <div className={styles.bars}>
+            <RxHamburgerMenu />
+          </div>
+        </div>
+        <NavLink to={"/"}>
+          <div className={styles.logo}>
+            <FaHome />
+          </div>
+          <div className={styles.text}>Home</div>
+        </NavLink>
+        <NavLink to={"/about"}>
+          <div className={styles.logo}>
+            <FaHome />
+          </div>
+          <div className={styles.text}>About</div>
+        </NavLink>
+        <NavLink to={"/menu"}>
+          <div className={styles.logo}>
+            <FaHome />
+          </div>
+          <div className={styles.text}>Menu</div>
+        </NavLink>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
 };
+
+{
+  /* <NavLink to={"/"}>
+<FaHome />
+<div>Home</div>
+</NavLink>
+<NavLink to={"/about"}>
+<FaHome />
+<div>Abour</div>
+</NavLink>
+<NavLink to={"/menu"}>
+<FaHome />
+<div>Menu</div>
+</NavLink> */
+}
