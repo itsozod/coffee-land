d3b6b180a39f1e359a518fb4ffc5d850fd66d339
Ozod <o.bukhorizoda@mail.ru>
2023-12-14 10:22:01 +0500

    styled the home-page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Home.module.css";
 export const Home = () => {
   return (
     <section className={styles.section_home}>
-      <h1 className={styles.home_title}>Welcome to Coffeeland</h1>
+      <div className={styles.home_container}>
+        <img
+          className={styles.loader}
+          src="coffee-icon.png"
+          alt="Coffee-icon"
+        />
+        <h1 className={styles.home_title}>Welcome to Coffeeland</h1>
+        <h1 className={styles.home_info}>Experience Coffee</h1>
+        <h1 className={styles.home_info2}>like never before</h1>
+        <NavLink className={styles.menuBtnLink} to={"/menu"}>
+          Explore menu
+        </NavLink>
+      </div>
     </section>
   );
 };
