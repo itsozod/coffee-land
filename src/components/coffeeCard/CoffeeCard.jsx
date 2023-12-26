6c8ceb14ce393b43e26cdbfcd587c84fa04d2ea6
Ozod <o.bukhorizoda@mail.ru>
2023-12-26 16:09:16 +0500

    created a coffee card in menu page

---
+++ src/components/coffeeCard/CoffeeCard.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import styles from "./CoffeeCard.module.css";
+
+export const CoffeeCard = ({coffee}) => {
+  return (
+    <article className={styles.coffee_card}>
+      <p className={styles.coffee_title}>{coffee.title}</p>
+      <img className={styles.coffee_img} src={coffee.img} alt={coffee.title} />
+      <h3>${coffee.price}</h3>
+    </article>
+  );
+};
+
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,3 +1,36 @@
+import { CoffeeCard } from "../../components/coffeeCard/CoffeeCard";
+import styles from "./Menu.module.css";
+import { useState, useEffect } from "react";
 export const Menu = () => {
-  return <h1>Menu Page</h1>;
+  const [coffees, setCoffees] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getCoffees = async () => {
+      try {
+        console.log("Coffees");
+        setLoading(true);
+        const response = await fetch("http://localhost:3000/coffees");
+        const data = await response.json();
+        setCoffees(data);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getCoffees();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className={styles.coffee_container}>
+          {coffees.map((coffee) => (
+            <CoffeeCard key={coffee.id} coffee={coffee} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
