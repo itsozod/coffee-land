31907c8df3281105785b5c38b60c58cc1911d408
Ozod <o.bukhorizoda@mail.ru>
2023-12-30 23:14:57 +0500

    made a basic booking page

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -1,17 +1,14 @@
 import styles from "./Booking.module.css";
-// const images = ["black-coffee.jpg", "cappucino.jpg", "americano.jpg"];
-// import { useState } from "react";
+const images = ["dinner-table-design.png", "wooden-table.png"];
+const food = ["egg-salmon-avocado.png"];
+import { useState } from "react";
 export const Booking = () => {
-  // const [image, setImage] = useState("");
+  const [image, setImage] = useState("");
+  const [foods, setFoods] = useState("");
   return (
     <section className={styles.booking_section}>
       <h1 style={{ position: "relative", color: "#fff" }}>Book a table</h1>
-    </section>
-  );
-};
-
-{
-  /* <div className={styles.img_container}>
+      <div className={styles.img_container}>
         {images.map((img) => (
           <button
             className={styles.btn_img}
@@ -22,10 +19,28 @@ export const Booking = () => {
           </button>
         ))}
       </div>
+      {food.map((food) => (
+        <img
+          style={{ width: "130px", position: "relative" }}
+          key={food}
+          src={food}
+          onClick={() => setFoods(food)}
+        ></img>
+      ))}
 
       {image ? (
-        <img className={styles.booked_img} src={image} alt="Image"></img>
+        <div>
+          <img className={styles.booked_img} src={image} alt="Image"></img>
+          {foods && (
+            <img
+              style={{ position: "absolute", top: "65%", left: "45%", width: "130px" }}
+              src={food}
+            ></img>
+          )}
+        </div>
       ) : (
         ""
-      )} */
-}
+      )}
+    </section>
+  );
+};
