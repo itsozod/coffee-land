ff2e43100a14c66011bf015cc31f4c487672ecd1
Ozod <o.bukhorizoda@mail.ru>
2024-01-01 14:39:48 +0500

    install mui and styled book button

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -5,10 +5,28 @@ import { Tables } from "../../components/tables/Tables";
 import { DishesMenu } from "../../components/dishesMenu/DishesMenu";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { Button, Snackbar, Alert } from "@mui/material";
+
 export const Booking = () => {
   const [tablesImg, setTablesImg] = useState("");
   const [menuFood, setMenuFood] = useState("");
   const [rotate, setRotate] = useState(false);
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const handleBookBtn = () => {
+    setOpenSnackBar(true);
+  };
+
+  const handleCloseSuccess = () => {
+    setOpenSnackBar(false);
+    setTimeout(() => {
+      setTablesImg("");
+      setMenuFood("");
+    }, 1500);
+  };
+  const handleCloseError = () => {
+    setOpenSnackBar(false);
+  };
   return (
     <section className={styles.booking_section}>
       <h1 style={{ position: "relative", color: "#fff" }}>Book a table</h1>
@@ -60,7 +78,47 @@ export const Booking = () => {
         <div className={styles.booked_img_container}>
           {menuFood && <img className={styles.table_dish} src={menuFood}></img>}
           <img className={styles.booked_img} src={tablesImg} alt="Image"></img>
-          <button>Book</button>
+          <div className={styles.book_table_btn_container}>
+            <Button
+              sx={{
+                backgroundColor: "aqua",
+                ":hover": { backgroundColor: "aquamarine" },
+              }}
+              onClick={() => handleBookBtn()}
+            >
+              Book Table
+            </Button>
+            {menuFood ? (
+              <Snackbar
+                open={openSnackBar}
+                autoHideDuration={6000}
+                onClose={handleCloseSuccess}
+              >
+                <Alert
+                  onClose={handleCloseSuccess}
+                  severity="success"
+                  sx={{ width: "100%" }}
+                >
+                  Your table order was accepted successfully, we will contact
+                  you!
+                </Alert>
+              </Snackbar>
+            ) : (
+              <Snackbar
+                open={openSnackBar}
+                autoHideDuration={6000}
+                onClose={handleCloseError}
+              >
+                <Alert
+                  onClose={handleCloseError}
+                  severity="error"
+                  sx={{ width: "100%" }}
+                >
+                  Choose a dish before booking your table!
+                </Alert>
+              </Snackbar>
+            )}
+          </div>
         </div>
       ) : (
         ""
