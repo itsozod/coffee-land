89fe5f12e2ead03f28b05def31095e6787f36af6
Ozod <o.bukhorizoda@mail.ru>
2024-01-04 14:21:19 +0500

    made a basic cart logic

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux";
 import styles from "./Cart.module.css";
 
 export const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
   return (
     <section className={styles.order_section}>
-      <h1>No items were added yet</h1>
+      {cart.length === 0 && <p>No items were added yet!</p>}
+      {cart.map((item) => (
+        <article key={item.id}>
+          <p>{item.coffeeName}</p>
+          <img style={{ width: "100px" }} src={item.coffeeCupImg} alt="" />
+        </article>
+      ))}
     </section>
   );
 };
