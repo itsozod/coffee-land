cfec9ec95b9e2a136367375839b8c27f409f24bc
Ozod <o.bukhorizoda@mail.ru>
2024-01-05 19:06:12 +0500

    styled the cart when it is empty

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import styles from "./Cart.module.css";
 import { useDarkMode } from "../../hooks/darkmodeHook/UseDarkMode";
+import { NavLink } from "react-router-dom";
 
 export const Cart = () => {
   const cart = useSelector((state) => state.cart.cart);
@@ -15,14 +16,31 @@ export const Cart = () => {
           transition: ".3s",
         }}
       >
-        {cart.length === 0 && <p>No items were added yet!</p>}
-        {cart.map((item) => (
-          <article key={item.id} className={styles.cart_card}>
-            <p>{item.title}</p>
-            <p>{item.price}</p>
-            <img className={styles.cart_img} src={item.img} alt="" />
-          </article>
-        ))}
+        {cart.length === 0 && (
+          <div
+            className={styles.empty_cart}
+            style={{
+              color: darkMode ? "white" : "black",
+              transition: ".3s",
+            }}
+          >
+            <h3>No items were added yet!</h3>
+            <button className={styles.empty_btn}>
+              <NavLink className={styles.empty_link} to={"/menu"}>
+                Visit our menu page to order!
+              </NavLink>
+            </button>
+          </div>
+        )}
+        <div className={styles.cart_container}>
+          {cart.map((item) => (
+            <article key={item.id} className={styles.cart_card}>
+              <p>{item.title}</p>
+              <p>{item.price}</p>
+              <img className={styles.cart_img} src={item.img} alt="" />
+            </article>
+          ))}
+        </div>
       </section>
     </>
   );
